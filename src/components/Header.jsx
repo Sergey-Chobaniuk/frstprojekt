bbb42b9288d5cb0feb4bf71a77ba225203e44453
sergey <chobaniukinmarket@gmail.com>
2022-03-19 16:18:20 +0300

    first comiit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Header = () =>{
+    return <header className='header'>
+        <div>
+            {/*<img src='https://w7.pngwing.com/pngs/803/598/png-transparent-phoenix-logo-phoenix-red-bird-illustration-leaf-photography-mirror.png'/>*/}
+            <img src={'https://upload.wikimedia.org/wikipedia/ru/b/b2/Hatayspor_-_logo.png'}/>
+        </div>
+    </header>
+}
+export default Header;
