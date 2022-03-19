2e33c0636c030bd1148a96e4d53144cf0c700451
ИÐSergey Chobaniuk <chobaniukinmarket@gmail.com>
2022-03-20 00:07:15 +0300

    ÐLearn 14 name.module.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import h from './Header.module.css';
 const Header = () =>{
-    return <header className='header'>
+    return <header className={h.header}>
         <div>
             {/*<img src='https://w7.pngwing.com/pngs/803/598/png-transparent-phoenix-logo-phoenix-red-bird-illustration-leaf-photography-mirror.png'/>*/}
             <img src={'https://upload.wikimedia.org/wikipedia/ru/b/b2/Hatayspor_-_logo.png'}/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,1 @@
-import React from "react";
-const Navbar = () =>{
-    return <nav className={'nav'}>
-        <div>
-            <a>Profile</a>
-        </div>
-        <div>
-            <a>Massages</a>
-        </div>
-        <div>
-            <a>News</a>
-        </div>
-        <div>
-            <a>Musick</a>
-        </div>
-        <div>
-            <a>Settings</a>
-        </div>
-    </nav>
-}
-export default Navbar;
+import React from "react";import n from './Navbar.module.css';const Navbar = () =>{    return <nav className={n.nav}>        <div>            <a>Profile</a>        </div>        <div>            <a>Massages</a>        </div>        <div>            <a>News</a>        </div>        <div>            <a>Musick</a>        </div>        <div>            <a>Settings</a>        </div>    </nav>}export default Navbar;
