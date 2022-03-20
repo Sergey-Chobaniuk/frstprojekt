75d72cbb623d422b180369bcdb86d6096c572443
ИÐSergey Chobaniuk <chobaniukinmarket@gmail.com>
2022-03-20 23:15:48 +0300

    learn 16 Add props

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import Post from './Post/Post.jsx';
 import './MyPosts.module.css';
-const MyPosts = () =>{
+const MyPosts = () => {
     return(
         <div>
             My Post
             <div>
                 {/*New Posts*/}
-                <textarea></textarea>
+                <textarea> </textarea>
                 <button>Add post</button>
             </div>
-            <Post />
-            <Post />
+            <Post message={'Hi, how are you?'} Number_of_likes={'15'} />
+            <Post message={"It's my first post"} Number_of_likes={"20"} />
             <Post />
             <Post />
             <Post />
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,14 +1,16 @@
 import c from "./Post.module.css";
 import React from "react";
-const Post = () =>{
+const Post = (props) =>{
+    console.log(props.message);
     return (
         <div className={c.item}>
             <div>
                 <img src={'https://salvemusic.com.ua/wp-content/uploads/2020/02/ava-max.jpg'}/>
             </div>
-            Post 1,2,3,..
+            {props.message}
             <div>
                 <span>like</span>
+                {props.Number_of_likes}
             </div>
         </div>
     )
