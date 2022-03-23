890dd69d4f23023cd4050c0a16995b9fc29b8c5e
ИÐSergey Chobaniuk <chobaniukinmarket@gmail.com>
2022-03-23 23:21:40 +0300

    Learn 21 layout of the dialog pageÐ

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,8 +1,42 @@
 import React from "react";
-import s from './Dialogs.module.css';
-const Dialogs =() => {
-    return <div>
-        <a>Dialogs</a>
+import d from './Dialogs.module.css';
+const Dialogs =(props) => {
+    return (
+    <div className={d.dialogs}>
+        <div className={d.dialogItems}>
+            <div className={d.dialog}>
+                Dimych
+            </div>
+            <div className={d.dialog}>
+                Vasia
+            </div>
+            <div className={d.dialog}>
+                Pitia
+            </div>
+            <div className={d.dialog}>
+                Anna
+            </div>
+            <div className={d.dialog}>
+                Vika
+            </div>
+            <div className={d.dialog}>
+                Lena
+            </div>
+        </div>
+        <div className={d.messages}>
+            <div className={d.message}>
+                Haw a you?
+            </div>
+            <div className={d.message}>
+                I say to you
+            </div>
+            <div className={d.message}>
+                My name is Dimych
+            </div>
+        </div>
     </div>
+
+    )
+
 }
 export default Dialogs;
