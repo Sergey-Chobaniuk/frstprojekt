1625e41ef274c8bd6e13b42d2a75465afac97f7e
ИÐSergey Chobaniuk <chobaniukinmarket@gmail.com>
2022-03-26 22:59:56 +0300

    learn 22 Add NavLink for dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 import d from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 const Dialogs =(props) => {
     return (
     <div className={d.dialogs}>
         <div className={d.dialogItems}>
             <div className={d.dialog}>
-                Dimych
+                <NavLink to={'/dialogs/1'}>Dimich</NavLink>
             </div>
             <div className={d.dialog}>
-                Vasia
+                <NavLink to={'/dialogs/2'}>Vasia</NavLink>
             </div>
             <div className={d.dialog}>
-                Pitia
+                <NavLink to={'/dialogs/3'}>Pitia</NavLink>
             </div>
             <div className={d.dialog}>
-                Anna
+                <NavLink to={'/dialogs/4'}>Anna</NavLink>
             </div>
             <div className={d.dialog}>
-                Vika
+                <NavLink to={'/dialogs/5'}>Vika</NavLink>
             </div>
             <div className={d.dialog}>
-                Lena
+                <NavLink to={'/dialogs/6'}>Lena</NavLink>
             </div>
         </div>
         <div className={d.messages}>
