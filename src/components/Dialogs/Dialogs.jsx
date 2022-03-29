dd34ab2dc172be99a79c22ee4f5db5d6669a84fd
ИÐSergey Chobaniuk <chobaniukinmarket@gmail.com>
2022-03-29 23:25:02 +0300

    learn 25 array.map

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,40 +1,50 @@
 import React from "react";
 import d from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
-const DialogItem =(props)=> {
-    let path = "/dialogs/"+props.id;
-    return(
+
+const DialogItem = (props) => {
+    let path = "/dialogs/" + props.id;
+    return (
         <div className={d.dialog}>
             <NavLink to={path}>{props.name}</NavLink>
         </div>
     )
 }
-const Message =(props)=>{
-    return(
+const Message = (props) => {
+    return (
         <div className={d.message}>
             {props.message}
         </div>
     )
 }
-const Dialogs =(props) => {
+const Dialogs = (props) => {
+    let dialogs = [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Vasia'},
+        {id: 3, name: 'Pitia'},
+        {id: 4, name: 'Anna'},
+        {id: 5, name: 'Vika'},
+        {id: 6, name: 'Lena'},
+    ]
+    let dialogElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
+    let messages = [
+        {id: 1, message: 'Haw a you?'},
+        {id: 2, message: 'I say to you'},
+        {id: 3, message: 'My name is'},
+        {id: 4, message: 'Haw do you do?'},
+        {id: 5, message: 'What?'},
+    ]
+    let messageElements = messages.map(m => <Message message={m.message}/>);
+
     return (
-    <div className={d.dialogs}>
-        <div className={d.dialogItems}>
-            <DialogItem name={"Dimych"} id={"1"} />
-            <DialogItem name={"Vasia"} id={"2"} />
-            <DialogItem name={"Pitia"} id={"3"} />
-            <DialogItem name={"Anna"} id={"4"} />
-            <DialogItem name={"Vika"} id={"5"} />
-            <DialogItem name={"Lena"} id={"6"} />
+        <div className={d.dialogs}>
+            <div className={d.dialogItems}>
+                {dialogElements}
+            </div>
+            <div className={d.messages}>
+                {messageElements}
+            </div>
         </div>
-        <div className={d.messages}>
-            <Message message={"Haw a you?"} />
-            <Message message={"I say to you"} />
-            <Message message={"My name is"} />
-            <Message message={"Haw do you do?"} />
-            <Message message={"What?"} />
-        </div>
-    </div>
     )
 }
 export default Dialogs;
